500b37322f16808d2c37dc5ccc80e4b1c0df5852
Alexander Fedoniuk <35227010+whodeen@users.noreply.github.com>
2020-09-24 00:30:13 +0300

    Added MAP SECTION script+styling

---
+++ scripts/map.js
@@ -0,0 +1,30 @@
+"use strict";
+
+function initMap() {
+  const markerCoordinates = { lat: 59.938789, lng: 30.323069 };
+  const centerCoordinates = { lat: 59.939194, lng: 30.329530 };
+
+  let map = new google.maps.Map(document.getElementById("map"), {
+    center: centerCoordinates,
+    zoom: 15.9,
+    clickableIcons: false,
+    fullscreenControl: false,
+    mapTypeControl: false,
+    streetViewControl: false,
+    zoomControl: false,
+  });
+
+  let marker = new google.maps.Marker({
+    position: markerCoordinates,
+    map: map,
+    icon: 'img/map_pin.svg',
+  });
+
+  console.log(marker);
+  console.log(map);
+}
+
+//,
+
+
+//59.939194, 30.329530
